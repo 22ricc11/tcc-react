50a18499d44822e55a6044ff94828e8cae7ac4c0
Marilia <ra54168583899@acaonsfatima.org.br>
2023-10-07 16:20:35 -0300

    a

--- src/pages/cadastro/cadastro.js
+++ src/pages/cadastro/cadastro.js
@@ -9,37 +9,43 @@ export default function Cadastro(){
     const[senha, setSenha] = useState();
     const[nome, setNome] = useState();
     const[confS, setConfS] = useState();
+    const[senhaV, setsenhaV] = useState(false);
+
+    const toggleVisibilidadeSenha = () => {
+        setsenhaV(!senhaV);
+    }
 
     return(
         <div className='principal'>
+            <div className='PP'>
 
-            <img src='/assets/images/login.png' alt=''/>
+                <img id='ima' src='/assets/images/login.png' alt=''/>
 
-            <div className='bt'>
-                <Link to='/login'>Login</Link>
-            </div>
+                <Link id='bt' to='/login'>Login</Link>
 
-            <div className='coisas'>
+                <div className='coisas'>
+                    <div className='cc'>
+                        <h1>Criar <br></br>Conta</h1>
+                    </div>
 
-                <div className='cc'>
-                    <h1>Criar <br></br>Conta</h1>
+                    <div>
+                        <input type="text" value={nome} placeholder='Nome' onChange={e=> setNome(e.target.value)} />
+                    </div>
+                    <div>
+                        <input type='text' value={email} placeholder='E-mail' onChange={e=> setEmail(e.target.value)} />
+                    </div>
+                    <div>
+                        <input type={senhaV ? 'text' : 'password'} value={senha} placeholder='Senha' onChange={e=> setSenha(e.target.value)} />
+                    </div>
+                    <div>
+                        <input type={senhaV ? 'text' : 'password'} value={confS} placeholder='Confirmar senha' onChange={e=> setConfS(e.target.value)} />
+                    </div>
+
+                    <button onClick={toggleVisibilidadeSenha}> {senhaV ? <img src='/assets/images/olhoA.png' alt=''/> : <img src='/assets/images/olhoF.png' alt=''/> }</button>
+                    
+                    <a href='/'><button>confirmar</button></a>
                 </div>
 
-                <div>
-                    <input type="text" value={nome} placeholder='Nome' onChange={e=> setNome(e.target.value)} />
-                </div>
-                <div>
-                    <input type='text' value={email} placeholder='E-mail' onChange={e=> setEmail(e.target.value)} />
-                </div>
-                <div>
-                    <input type="text" value={senha} placeholder='Senha' onChange={e=> setSenha(e.target.value)} />
-                </div>
-                <div>
-                    <input type='text' value={confS} placeholder='Confirmar senha' onChange={e=> setConfS(e.target.value)} />
-                </div>
-                
-
-                <a href='/'><button>confirmar</button></a>
             </div>
         </div>
     )
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -6,7 +6,6 @@ import { useState } from 'react';
 export default function Login(){
 
     const[emailL, setEmailL] = useState();
-
     const[senhaV, setsenhaV] = useState(false)
     const[senhaL, setSenhaL] = useState();
 
