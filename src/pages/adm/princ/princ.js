7f27afa123bdd4e07788a21ccb7c55269a813c86
Marilia <ra54168583899@acaonsfatima.org.br>
2023-10-10 08:20:43 -0300

    a

--- src/pages/adm/princ/princ.js
+++ src/pages/adm/princ/princ.js
@@ -1,1 +1,40 @@
+import { Link } from 'react-router-dom'
+import './princ.scss'
 
+export default function Princ(){
+
+    return(
+        <div className='box'>
+            <div className='cabecalho'>
+                <img src='' alt=''/>
+
+                <img src='./assets/images/perfil-adm.png' alt='' />
+                <p>Olá Sr.</p>
+            </div>
+
+            <div className='meio'>
+                <div className='a'>
+                    <div>
+                        <img src='' alt=''/>
+                        <Link to=''>Cadastro de Produto</Link>
+                    </div>
+                    <div>
+                        <img src='' alt=''/>
+                        <Link to=''>Gráfico</Link>
+                    </div>
+                </div>
+
+                <div className='b'>
+                    <div>
+                        <img src='' alt=''/>
+                        <Link to=''></Link>
+                    </div>
+                    <div>
+                        <img src='' alt=''/>
+                        <Link to=''>Em Construção</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
